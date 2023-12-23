9441136a1759cf48f7daa5e549c7d4cea6355d0f
Divu <divyeshkuchhadia@gmail.com>
2023-12-23 16:05:22 +0530

    higher order function #3.2.2

--- LLHOF/app/src/main/java/com/littlelemon/llhof/MainActivity.kt
+++ LLHOF/app/src/main/java/com/littlelemon/llhof/MainActivity.kt
@@ -4,13 +4,9 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.littlelemon.llhof.ui.theme.LLHOFTheme
 
@@ -27,7 +23,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun SimpleUI(onClick: (Int) -> Unit) {
-    var timesClicked : Int = 1
+    var timesClicked = 1
     Button(onClick = { onClick(timesClicked++) }) {
         Text(text = "Button")
     }
