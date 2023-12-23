5d0492f2a554fdb0ab10dd7f3ab80e4993e37df8
Divu <divyeshkuchhadia@gmail.com>
2023-12-23 16:03:06 +0530

    higher order function #3.2.2

---
+++ LLHOF/app/src/main/java/com/littlelemon/llhof/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.littlelemon.llhof
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.littlelemon.llhof.ui.theme.LLHOFTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SimpleUI {timesClicked ->
+                Toast.makeText(this,"Clicked $timesClicked",Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
+
+@Composable
+fun SimpleUI(onClick: (Int) -> Unit) {
+    var timesClicked : Int = 1
+    Button(onClick = { onClick(timesClicked++) }) {
+        Text(text = "Button")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LLHOFTheme {
+
+    }
+}
