1ad103b3856af35059ffdab0c5408849ee67aee8
Divu <divyeshkuchhadia@gmail.com>
2024-02-17 17:05:35 +0530

    commit before excercise 4.2.6 start

--- SharedPreferences Start/app/src/main/java/com/example/littlelemon/MainActivity.kt
+++ SharedPreferences Start/app/src/main/java/com/example/littlelemon/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.littlelemon
 
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
@@ -19,9 +19,15 @@ import com.example.littlelemon.ui.theme.LittleLemonTheme
 
 class MainActivity : ComponentActivity() {
 
+    private val tipMenuLiveData = MutableLiveData<Boolean>()
+    private val sharedPreferences : SharedPreferences by lazy {
+        applicationContext.getSharedPreferences("LittleLemon", MODE_PRIVATE)
+    }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        tipMenuLiveData.value = sharedPreferences.getBoolean("Tip",false)
         setContent {
             LittleLemonTheme {
                 Surface(
@@ -29,6 +35,13 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.Center) {
                         Text(text = "Add Tip?")
+                        val selected = tipMenuLiveData.observeAsState(false)
+                        Switch(checked = selected.value, onCheckedChange = {
+                            sharedPreferences.edit(commit = true){
+                                putBoolean("Tip",it)
+                            }
+                            runOnUiThread{tipMenuLiveData.value=it}
+                        })
                     }
                 }
             }
